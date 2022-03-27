3a120329511dff20e1c1921e431019c492cd270b
Lara <laraayumi56@gmail.com>
2022-03-27 19:42:10 -0300

    Comportamento do attacker implementado

--- src/attacker.c
+++ src/attacker.c
@@ -1,5 +1,8 @@
 // Standard headers
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 // Internal headers
 #include "direction.h"
@@ -9,6 +12,8 @@
 // Main header
 #include "attacker.h"
 
+// Added *
+
 // Macros
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
 
@@ -16,14 +21,134 @@
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
+enum {up, down};
+
+int sorteia_numero() {
+  return rand();
+}
+
+bool equal_directions(direction_t d1, direction_t d2) {
+  if(d1.i == d2.i && d1.j == d2.j)
+    return true;
+
+  return false;
+}
+
+void atualiza_ult_dir(direction_t ultima_dir[3], direction_t atual) {
+  ultima_dir[2] = ultima_dir[1];
+  ultima_dir[1] = ultima_dir[0];
+  ultima_dir[0] = atual;
+}
+
 direction_t execute_attacker_strategy(
     position_t attacker_position, Spy defender_spy) {
   // TODO: unused parameters, remove these lines later
-  UNUSED(attacker_position);
   UNUSED(defender_spy);
 
   // TODO: Implement Attacker logic here
-  return (direction_t) DIR_RIGHT;
+  int aux; 
+  int seed;
+  direction_t result;
+  static int rodada = 0;
+  static int rounds_parado = 0;
+  static direction_t ini_dir;
+  static direction_t ultima_dir[3];
+  static position_t ultima_pos;
+
+  if(!rodada) {
+    srand(time(0));
+    seed = rand()%2;
+
+    ultima_dir[2] = ultima_dir[1] = ultima_dir[0] = (direction_t) DIR_STAY;
+
+    switch(seed){
+      case up: 
+        ini_dir = (direction_t) DIR_UP;
+        break;
+
+      case down:
+        ini_dir = (direction_t) DIR_DOWN;
+        break;
+
+      default:
+        ini_dir = (direction_t) DIR_STAY;
+    }
+
+    result = ini_dir;
+  }
+
+  else {
+
+    if(equal_positions(ultima_pos, attacker_position)) {
+      rounds_parado++;
+
+      switch(rounds_parado) {
+        case 1:
+
+          if (equal_directions(ultima_dir[0], (direction_t) DIR_UP) ||
+              equal_directions(ultima_dir[0], (direction_t) DIR_DOWN)) {
+              if(equal_directions(ultima_dir[1], (direction_t) DIR_LEFT))
+                result = (direction_t) DIR_LEFT;
+              else 
+                result = (direction_t) DIR_RIGHT;
+            }
+
+          
+          else {
+            if(equal_directions(ini_dir, (direction_t) DIR_DOWN))
+              result = (direction_t) DIR_UP;
+            else
+              result = (direction_t) DIR_DOWN;
+          }
+          break;
+
+        case 2:
+          if (equal_directions(ultima_dir[0], (direction_t) DIR_UP))
+            result = (direction_t) DIR_DOWN;
+          else if (equal_directions(ultima_dir[0], (direction_t) DIR_DOWN))
+            result = (direction_t) DIR_UP;
+          else
+            result = (direction_t) DIR_LEFT;
+          break;
+          
+        case 3:
+          if (equal_directions(ultima_dir[3], (direction_t) DIR_UP))
+            result = (direction_t) DIR_DOWN;
+          else if (equal_directions(ultima_dir[3], (direction_t) DIR_DOWN))
+            result = (direction_t) DIR_UP;
+          else if (equal_directions(ultima_dir[3], (direction_t) DIR_RIGHT))
+            result = (direction_t) DIR_LEFT;
+          else result = (direction_t) DIR_RIGHT;
+          break;
+
+        default:
+          printf("Erro. Valor inesperado.\n");
+          result = (direction_t) DIR_STAY;
+      }
+
+    }
+
+    else {
+      rounds_parado = 0;
+
+      if(equal_directions(ultima_dir[0], (direction_t) DIR_LEFT)) {
+        aux = rand()%2;
+        if (aux == up)
+          result = (direction_t) DIR_UP;
+        else
+          result = (direction_t) DIR_DOWN;
+
+      } else
+        result = ultima_dir[0];
+    }
+
+  }
+
+  rodada++;
+  ultima_pos = attacker_position;
+  atualiza_ult_dir(ultima_dir, result);
+
+  return result;
 }
 
 /*----------------------------------------------------------------------------*/
--- src/defender.c
+++ src/defender.c
@@ -1,8 +1,12 @@
 // Standard headers
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 // Internal headers
 #include "direction.h"
+#include "attacker.h"
 #include "position.h"
 #include "spy.h"
 
@@ -12,18 +16,57 @@
 // Macros
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
 
+enum {up, down};
+
 /*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
+bool equal_directions_d(direction_t d1, direction_t d2) {
+  if(d1.i == d2.i && d1.j == d2.j)
+    return true;
+
+  return false;
+}
 
 direction_t execute_defender_strategy(
     position_t defender_position, Spy attacker_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(defender_position);
-  UNUSED(attacker_spy);
 
   // TODO: Implement Defender logic here
-  return (direction_t) DIR_LEFT;
+  //static int seed;
+  static direction_t result;
+  static position_t ultima_pos;
+  static direction_t ultima_dir;
+  static position_t attacker_position;
+  static bool stay;
+  static int rodada = 0;
+
+  if(!rodada) {
+    srand(time(0));
+    //seed = rand()%2;
+
+    if(rand()%2 == up)
+      result = (direction_t) DIR_UP;
+    else
+      result = (direction_t) DIR_DOWN;
+
+  }
+
+  else {
+    if(equal_positions(ultima_pos, defender_position)) {
+
+      if(equal_directions_d(ultima_dir,(direction_t) DIR_UP))
+        result = (direction_t) DIR_DOWN;
+      else
+        result = (direction_t) DIR_UP;
+    }
+    else
+      result = ultima_dir;
+  }
+
+  rodada++;
+  ultima_dir = result;
+  ultima_pos = defender_position;
+  return result;
 }
 
 /*----------------------------------------------------------------------------*/
