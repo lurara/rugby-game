fffe301473b71ac14f2548fdb8130b863604541e
Lara <laraayumi56@gmail.com>
2022-03-27 21:40:00 -0300

    Defensor implementado

--- src/defender.c
+++ src/defender.c
@@ -1,7 +1,5 @@
 // Standard headers
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
 #include <stdbool.h>
 
 // Internal headers
@@ -31,36 +29,69 @@ bool equal_directions_d(direction_t d1, direction_t d2) {
 direction_t execute_defender_strategy(
     position_t defender_position, Spy attacker_spy) {
 
-  // TODO: Implement Defender logic here
-  //static int seed;
   static direction_t result;
   static position_t ultima_pos;
+  static position_t primeira_pos;
   static direction_t ultima_dir;
   static position_t attacker_position;
   static bool stay;
   static int rodada = 0;
 
   if(!rodada) {
-    srand(time(0));
-    //seed = rand()%2;
+    result = (direction_t) DIR_LEFT;
+    primeira_pos = defender_position;
+  }
 
-    if(rand()%2 == up)
+  else if(rodada == 1) {
+
+    attacker_position = get_spy_position(attacker_spy);
+
+    stay = true;
+
+    if(attacker_position.i == defender_position.i) {
+      result = (direction_t) DIR_LEFT;
+      stay = false;
+    }
+
+    else if (attacker_position.i < defender_position.i) {
       result = (direction_t) DIR_UP;
-    else
+    }
+
+    else {
       result = (direction_t) DIR_DOWN;
+    }
 
   }
 
   else {
-    if(equal_positions(ultima_pos, defender_position)) {
+
+    if (equal_positions(ultima_pos, defender_position)) {
 
       if(equal_directions_d(ultima_dir,(direction_t) DIR_UP))
         result = (direction_t) DIR_DOWN;
       else
         result = (direction_t) DIR_UP;
     }
-    else
-      result = ultima_dir;
+
+    else {
+
+      size_t atual;
+
+      if(equal_directions_d(ultima_dir, (direction_t) DIR_UP))
+        atual = ultima_pos.i - 1;
+      else
+        atual = ultima_pos.i + 1;
+
+      if (stay && atual == primeira_pos.i) {
+        if (equal_directions_d(ultima_dir, (direction_t) DIR_UP))
+          result = (direction_t) DIR_DOWN;
+        else 
+          result = (direction_t) DIR_UP;
+      }
+
+      else 
+        result = ultima_dir;
+    }
   }
 
   rodada++;
